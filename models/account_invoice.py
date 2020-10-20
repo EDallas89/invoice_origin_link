12f639a0e0b6582e706fb0969b122f9f1094f45e
EDallas <inmsanz89@gmail.com>
2020-10-20 13:57:32 +0100

    IMP link on Vender Bills and clean extra code

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -5,12 +5,18 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    origin_m2o = fields.Many2one(
+    origin_m2o_so = fields.Many2one(
         comodel_name='sale.order',
+        compute='_compute_origin',
+    )
+
+    origin_m2o_po = fields.Many2one(
+        comodel_name='purchase.order',
         compute='_compute_origin',
     )
 
     @api.one
     @api.depends('origin')
     def _compute_origin(self):
-        self.origin_m2o = self.env['sale.order'].search([('name', '=', self.origin)], limit=1).id
+        self.origin_m2o_so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1).id
+        self.origin_m2o_po = self.env['purchase.order'].search([('name', '=', self.origin)], limit=1).id
--- models/sale_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    origin_o2m = fields.One2many(
-        comodel_name='account.invoice', 
-        inverse_name='origin_m2o',
-    )
