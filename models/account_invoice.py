59058535b60396dc325c6ad82e4a1dc0263d877b
EDallas <inmsanz89@gmail.com>
2020-10-22 12:04:45 +0100

    IMP Refactor module to use many2many instead mnay2one

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -5,32 +5,18 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-#    origin_m2o_so = fields.Many2one(
-#        comodel_name='sale.order',
-#        compute='_compute_origin',
-#    )
-#
-#    origin_m2o_po = fields.Many2one(
-#        comodel_name='purchase.order',
-#        compute='_compute_origin',
-#    )
-#
-#    origin_m2o_ai = fields.Many2one(
-#        comodel_name='account.invoice',
-#        compute='_compute_origin',
-#    )
-#
-#    @api.one
-#    @api.depends('origin')
-#    def _compute_origin(self):
-#            self.origin_m2o_so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1).id
-#            self.origin_m2o_po = self.env['purchase.order'].search([('name', '=', self.origin)], limit=1).id
-#            self.origin_m2o_ai = self.env['account.invoice'].search([('number', '=', self.origin)], limit=1).id
-
-
-    origin_m2m = fields.Many2many(
+    origin_m2m_so = fields.Many2many(
         comodel_name='sale.order',
-        string='Many2Many',
+        compute='_compute_many2many'
+    )
+
+    origin_m2m_po = fields.Many2many(
+        comodel_name='purchase.order',
+        compute='_compute_many2many'
+    )
+
+    origin_m2m_ai = fields.Many2many(
+        comodel_name='account.invoice',
         compute='_compute_many2many'
     )
     
@@ -39,4 +25,6 @@ class AccountInvoice(models.Model):
         if self.origin:
             origin_split = self.origin.split(", ")
             for origin in origin_split:
-                self.origin_m2m += self.env['sale.order'].search([('name', '=', origin)])
+                self.origin_m2m_so += self.env['sale.order'].search([('name', '=', origin)])
+                self.origin_m2m_po += self.env['purchase.order'].search([('name', '=', origin)])
+                self.origin_m2m_ai += self.env['account.invoice'].search([('number', '=', origin)])
