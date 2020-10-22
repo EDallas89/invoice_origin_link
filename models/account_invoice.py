c76a71d32f228221b63f956e7dbed28d73898d79
EDallas <inmsanz89@gmail.com>
2020-10-22 11:39:17 +0100

    computed many2many funtional

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -5,27 +5,38 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    origin_m2o_so = fields.Many2one(
+#    origin_m2o_so = fields.Many2one(
+#        comodel_name='sale.order',
+#        compute='_compute_origin',
+#    )
+#
+#    origin_m2o_po = fields.Many2one(
+#        comodel_name='purchase.order',
+#        compute='_compute_origin',
+#    )
+#
+#    origin_m2o_ai = fields.Many2one(
+#        comodel_name='account.invoice',
+#        compute='_compute_origin',
+#    )
+#
+#    @api.one
+#    @api.depends('origin')
+#    def _compute_origin(self):
+#            self.origin_m2o_so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1).id
+#            self.origin_m2o_po = self.env['purchase.order'].search([('name', '=', self.origin)], limit=1).id
+#            self.origin_m2o_ai = self.env['account.invoice'].search([('number', '=', self.origin)], limit=1).id
+
+
+    origin_m2m = fields.Many2many(
         comodel_name='sale.order',
-        compute='_compute_origin',
+        string='Many2Many',
+        compute='_compute_many2many'
     )
-
-    origin_m2o_po = fields.Many2one(
-        comodel_name='purchase.order',
-        compute='_compute_origin',
-    )
-
-    origin_m2o_ai = fields.Many2one(
-        comodel_name='account.invoice',
-        compute='_compute_origin',
-    )
-
-    origin_m2m = fields.Many2many(comodel_name='sale.order', string='Many2Many')
     
-
-    @api.one
     @api.depends('origin')
-    def _compute_origin(self):
-            self.origin_m2o_so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1).id
-            self.origin_m2o_po = self.env['purchase.order'].search([('name', '=', self.origin)], limit=1).id
-            self.origin_m2o_ai = self.env['account.invoice'].search([('number', '=', self.origin)], limit=1).id
+    def _compute_many2many(self):
+        if self.origin:
+            origin_split = self.origin.split(", ")
+            for origin in origin_split:
+                self.origin_m2m += self.env['sale.order'].search([('name', '=', origin)])
